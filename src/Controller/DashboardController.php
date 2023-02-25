8822dc8eb42fc19fc55820e64551e9737cb5a3b7
Duy Anh Nguyễn Xuân <anhnxd@famtech.vn>
2023-02-25 23:22:23 +0700

    first commit

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    public const DASHBOARD_ROUTE = 'app_dashboard';
+
+    #[Route('/dashboard', name: self::DASHBOARD_ROUTE)]
+    public function index(): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $users = $this->getDoctrine()->getRepository(User::class)->findByAnyRole();
+
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+            'users' => $users,
+        ]);
+    }
+}
